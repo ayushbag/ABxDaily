65a8b643abca790f72b25892d777a474259a9de8
Ayush <ayubag21@gmail.com>
2024-12-31 02:49:41 +0530

    text fix

--- frontend/src/pages/BlogsPage.jsx
+++ frontend/src/pages/BlogsPage.jsx
@@ -40,7 +40,7 @@ const BlogsPage = () => {
   return (
     <section className='w-full h-full bg-zinc-950'>
       {loading ? <LoadingComponent/> :
-      <div className='markdown-body max-w-4xl mx-auto p-4 sm:p-10 lg:p-12 bg-zinc-950'>
+      <div className='markdown-body text-white max-w-4xl mx-auto p-4 sm:p-10 lg:p-12 bg-zinc-950'>
         <ReactMarkdown>
           {blogContent}
         </ReactMarkdown>
